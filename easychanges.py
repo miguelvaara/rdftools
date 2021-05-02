08ba11e4d7e62bd3eb4308faeba9dcb76cbfac6b
Ahonen Mika J <mika.ahonen@helsinki.fi>
2021-05-02 04:39:33 +0300

    easychanges.py helps you to modify the content of RDF file

--- easychanges.py
+++ easychanges.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!../bin/python3
 # coding=utf-8
 import urllib.parse, sys, os, rdflib
 from rdflib import Graph, Namespace, URIRef, RDF, Literal
@@ -15,32 +15,35 @@ foaf = Namespace('http://xmlns.com/foaf/0.1/')
 ysa = Namespace('http://www.yso.fi/onto/ysa/')
 
 g = rdflib.Graph()
+g.parse(open(sys.argv[1], "r"), format="turtle")
 
-result = g.parse(file = open(sys.argv[1], "r"), format="turtle")
+# ******************************************************
+# Playground starts, comment the lines you do not need
 
-# Playground starts
-
-subj_1 = URIRef("http://urn.fi/URN:NBN:fi:au:ykl:00.1")
-g.add((subj_1, skos["hiddenLabel"], Literal('Hoppa på!', 'sv')))
+g.add((URIRef("http://urn.fi/URN:NBN:fi:au:ykl:00.1"), skos["hiddenLabel"], Literal('Hoppa på!', 'sv')))
+g.remove((None, skos["broader"], '<http://urn.fi/URN:NBN:fi:au:ykl:muotoluokka'))
 
 # g.query("SELECT * WHERE { ?v ?p ?o }")
 
-query = """
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-DELETE WHERE { 
-  ?sTest skos:broader <http://urn.fi/URN:NBN:fi:au:ykl:0> . 
-}
-"""
-
-g.update(query)
+# g.query("""
+# SELECT * WHERE {
+#         ?s ?p ?o .
+# }
+# """)
 
 # Playground ends
+# ******************************************************
+
+# If you used sparql
+# g.update(query)
 
 file_to_be_saved = open(sys.argv[2], "w+")
-
 file_to_be_saved.write(g.serialize(format="turtle").decode("utf-8"))
-
 file_to_be_saved.close()
 
 # Some instructions:
-# pip3 install rdflib
+# python3 -m venv ./vocabularies
+# cd vocabularies/
+# ../bin/pip3 install rdflib
+# ../bin/pip3 install requests
+#./easychanges.py source_file.ttl destination_file.ttl
